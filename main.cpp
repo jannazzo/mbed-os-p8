14af5a23cf766439f4096e1dc9413f1338b2697c
Luke Jannazzo <luke.jannazzo@gmail.com>
2026-04-14 20:27:08 +0000

    import relevant code segments from project 7

--- main.cpp
+++ main.cpp
@@ -1,12 +1,103 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+ 
 #include "mbed.h"
+#include <cstdio>
+#include <iostream>
+ 
+// PA_0 is CN8/A0
+AnalogIn TemperatureSensor(PA_1); //CN8/A1
+// PC_1 is CN8/A4
+InterruptIn START_BUTTON(PB_5); //CN5/D4
+InterruptIn STOP_BUTTON(PA_10); //CN5/D2
+DigitalOut OUTPUT(PB_4); //CN9/D5
+DigitalOut led(LED1);
+ 
+#define Vsupply 3.3f // The microcontroller supplies 3.3 V
+ 
+//variables for temperature sensor
+float TemperatureSensorDigiValue; //the A/D converter value read by the controller input pin
+float TemperatureSensorVoltValue; //the voltage on the controller input pin (across the 10k resistor) from the temperature sensor voltage divider
+float ThermistorResistance; //computed from the voltage drop value across the thermistor
+float ThermistorTemperature; //approximate ambient temperature measured by thermistor
+#define ThermistorBiasResistor 10000.0f //Bias resistor (lower leg of voltage divider) for thermistor
 
-// main() runs in its own thread in the OS
-int main()
+// Variables to hold control reference values.
+// STUDENT: EDIT THESE VALUES
+float TemperatureLimit = 26; //enter a temperature in Celsius here for temperature deactivation; NOTE: room temperature is 25C
+
+// This function will be attached to the start button interrupt.
+void StartPressed(void)
 {
-    while (true) {
+    cout << "Start!" << endl;
+    led = 1;
+    OUTPUT = 1;
+}
 
-        // code will go here
+// This function will be attached to the stop button interrupt.
+void StopPressed(void)
+{
+    // STUDENT: EDIT HERE
+    cout << "Stop!" << endl;
+    led = 0;
+    OUTPUT = 0;
+}
+ 
+// This function converts the voltage value from the thermistor input to an approximate temperature
+// in Celsius based on a linear approximation of the thermistor.
+float getThermistorTemperature(void)
+{
+	// STUDENT: EDIT HERE
+	// 1. Read the TemperatureSensor A/D value and store it in TemperatureSensorDigiValue
+	// 2. Calculate TemperatureSensorVoltValue from TemperatureSensorDigiValue and Vsupply
+	// 3. Calculate ThermistorResistance using the voltage divider equation
 
+    TemperatureSensorDigiValue = TemperatureSensor.read();
+    TemperatureSensorVoltValue = TemperatureSensorDigiValue*Vsupply;
+    ThermistorResistance = TemperatureSensorVoltValue*ThermistorBiasResistor/(Vsupply - TemperatureSensorVoltValue);
+
+    // changed order of bias and thermistor resistance to scale correctly
+    ThermistorTemperature = ((ThermistorResistance - 10000.0)/(1000.0)) + 25.0; //temperature of the thermistor computed by a linear approximation of the device response
+
+    return ThermistorTemperature;
+}
+ 
+// This function will check for a temperature triggered deactivation of the motor
+void CheckTemperatureSensor(void)
+{
+    // Use the getThermistorTemperature() function defined above to obtain a temperature value to use for comparison and decision making with your TemperatureLimit
+    if (getThermistorTemperature() >= TemperatureLimit) {
+        cout << "Temperature Stop!" << endl;
+        led = 0;
+        OUTPUT = 0;
     }
 }
 
+int main(void)
+{
+    // Setup an event queue to handle event requests for the ISR
+    // and issue the callback in the event thread.
+    EventQueue event_queue;
+    // Setup a event thread to update the motor
+    Thread event_thread(osPriorityNormal);
+    // Start the event on a loop ready to receive event calls from the event queue
+    event_thread.start(callback(&event_queue, &EventQueue::dispatch_forever));
+    // Attach the functions to the hardware interrupt pins to be inserted into
+    // the event queue and exicuted on button press.
+    START_BUTTON.rise(event_queue.event(&StartPressed));
+    STOP_BUTTON.rise(event_queue.event(&StopPressed));
+
+    while(true) {
+        // Check the analog inputs.
+        CheckTemperatureSensor();
+       
+        // output measured temperature for debugging
+        cout << "\rCurrent Temperature Value: " << getThermistorTemperature() << endl;
+        // cout << "\rStart Button: " << START_BUTTON.read() << endl;
+        // cout << "\rStop Button: " << STOP_BUTTON.read() << endl;
+       
+        wait_us(1000000); // Wait 1 second before repeating the loop.
+    }
+}
